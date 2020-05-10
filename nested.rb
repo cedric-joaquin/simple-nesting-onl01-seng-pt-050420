626638969dc442aa49279f26c38990f218147b06
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-10 06:21:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,12 +102,12 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = "#{alans_new_info}"
-     programmer_hash
-    # binding.pry
+    # programmer_hash
+    binding.pry
      
 end
 
-# changing_alan
+changing_alan
 
 def adding_to_dennis
 	programmer_hash = 
